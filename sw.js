76a3e07413c9b98627c512a58710b7edd433a691
Timeness <careless@flash.co>
2025-06-04 08:09:58 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("vyntra-cache").then(cache => {
+      return cache.addAll(["/", "/csindex.html", "/favicon.png"]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
